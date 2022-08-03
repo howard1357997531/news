117dcb44115a1f699ebd074bc4f1d444cf86a2ec
howard1357997531 <howard1357997531@yahoo.com.tw>
2022-08-03 19:33:16 +0800

    'modify'

--- news/settings.py
+++ news/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w9ib*_)w(x9ia!-vy0h+bmyij-o%n6rd6fkrs41s6k2ih+^h_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
